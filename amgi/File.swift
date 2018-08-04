7b7fea84df1699862115ccff95306f561d8efd47
neurrri <hannah_k3k@likelion.org>
2018-08-04 15:37:53 +0900

    Create contents
    단어식, 문장식 추가 및 보기

---
+++ amgi/File.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  amgi
+//
+//  Created by CAUAD09 on 2018. 8. 1..
+//  Copyright © 2018년 NEURRRI. All rights reserved.
+//
+
+import Foundation
+
+let wordData:WordData = WordData()
+let sentenceData:SentenceData = SentenceData()
+
+class WordData {
+    var allWords:[Words]
+    
+    init() {
+        self.allWords = []
+    }
+}
+
+
+class Words { //단어들
+    var keyword:String
+    var explanation:String
+    
+    init(keyword:String, explanation:String) {
+        self.keyword = keyword
+        self.explanation = explanation
+    }
+    
+}
+
+class SentenceData {
+    var allSentences : [String]
+    
+    init() {
+        self.allSentences = []
+    }
+}
+
